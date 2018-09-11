b3969eb8d961bdfc85fd02d48cd26451a5434bb4
Bigfish-Alex <632388230@qq.com>
2018-09-11 23:21:32 +0800

    会话插件增加TOAST模式，完成会话插件基本功能！

--- Zzc_Dialog/dialog.js
+++ Zzc_Dialog/dialog.js
@@ -8,8 +8,7 @@
             message:null,
             type:'normal',
             buttons:null,
-            delay:null,
-            maskOpacity:500
+            delay:null
         };
 
         // 默认参数扩展
@@ -30,6 +29,7 @@
         this.winContent=$('<div class="bf_dialog_content">');
         this.winFooter=$('<div class="bf_dialog_footer">');
         this.defaultButton=$('<button>是</button>');
+        this.toast=$('<div class="toast">');
 
         // 渲染Dom
         this.creat();
@@ -37,7 +37,6 @@
     Dialog.zIndex=10000;
     Dialog.prototype={
       creat:function(){
-        console.log(this.config)
         var _this_=this,
             config=this.config
             mask=this.mask,
@@ -47,6 +46,7 @@
             winContent=this.winContent,
             winFooter=this.winFooter,
             defaultButton=this.defaultButton,
+            toast=this.toast,
             body=this.body;
 
         if (!this.isConfig) {
@@ -57,38 +57,60 @@
             _this_.close();
           },3000)
         }else{
-          // 对话类型
-          if (config.type) {
-            win.addClass( config.type );
-            win.append( winHeader.html('提示') );
-          }
-          // 信息内容
-          if (config.message) {
-            win.append( winContent.html(config.message) );
-          }
-          // 按钮组
-          if (config.buttons){
-              this.creatButtons(winFooter,config.buttons)
+          // 判断对话类型
+          if(config.type=='toast'){
+              // TOAST
+              if (config.message) {
+                toast.html(config.message);
+              }
+              this.fadeIn();
+              body.append(toast);
+
+              // 自动关闭 默认3000ms
+              if ( config.delay && config.delay!=0 ){
+                setTimeout(function(){
+                  _this_.toast.remove();
+                },config.delay)
+              }else{
+                var delayTime=3000;
+                setTimeout(function(){
+                  _this_.toast.remove();
+                },delayTime)
+              }
           }else{
-            defaultButton.click(function(){
-              _this_.close();
-            })
-            winFooter.append(defaultButton);
-            win.append(winFooter);
+              if (config.type) {
+                win.addClass( config.type );
+                win.append( winHeader.html('提示') );
+              }
+              // 信息内容
+              if (config.message) {
+                win.append( winContent.html(config.message) );
+              }
+              // 按钮组
+              if (config.buttons){
+                  this.creatButtons(winFooter,config.buttons)
+              }else{
+                defaultButton.click(function(e){
+                  e.stopPropagation();
+                  _this_.close();
+                })
+                winFooter.append(defaultButton);
+                win.append(winFooter);
+              }
+              // 自动关闭
+              if ( config.delay && config.delay!=0 ){
+                setTimeout(function(){
+                  _this_.close();
+                },config.delay)
+              }
+              Dialog.zIndex++;
+              this.mask.css("z-index",Dialog.zIndex);
+              this.animate();
+              // 插入层
+              mask.append(win);
+              body.append(mask);
+            }
           }
-          // 自动关闭
-          if ( config.delay && config.delay!=0 ){
-            setTimeout(function(){
-              _this_.close();
-            },config.delay)
-          }
-          Dialog.zIndex++;
-          this.mask.css("z-index",Dialog.zIndex);
-          this.animate();
-          // 插入层
-          mask.append(win);
-          body.append(mask);
-        }
       },
       // 关闭对话
       close:function(){
@@ -105,13 +127,15 @@
           button.addClass(type);
           if(callback){
             button.click(function(e){
+              e.stopPropagation();
               var cb=callback();
               if(cb==undefined){
                 _this_.close();
               }
             })
           }else {
-            button.click(function(){
+            button.click(function(e){
+              e.stopPropagation();
               _this_.close();
             })
           }
@@ -126,7 +150,15 @@
         this.win.css("-webkit-transform","scale(0,0)");
         setTimeout(function(){
           _this_.win.css("-webkit-transform","scale(1,1)");
-        })
+        },10)
+      },
+      // 渐变出现效果
+      fadeIn:function(){
+        var _this_=this;
+        this.toast.css("opacity","0");
+        setTimeout(function(){
+          _this_.toast.css("opacity","1");
+        },10)
       }
     };
     window.Dialog=Dialog;
